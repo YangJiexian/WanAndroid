1a0eac6b9d1529d9e2f84820447cba4a40c499e5
qiudengjiao <qiudengjiao@gmail.com>
2019-01-02 22:53:55 +0800

    add initUI method

--- app/src/main/java/com/wanandroid/wanandroid/MainActivity.java
+++ app/src/main/java/com/wanandroid/wanandroid/MainActivity.java
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+    }
+
+    /**
+     * 初始化 UI
+     */
+    private void initUI() {
+
     }
 }
